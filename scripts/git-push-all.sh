41dadf01b8f68072a917c116d5825be0fed60c76
Julio Faerman <jufaerma@redhat.com>
2022-04-18 19:01:54 +0200

    faermanj WIP at 20220418-190154

--- scripts/git-push-all.sh
+++ scripts/git-push-all.sh
@@ -3,8 +3,6 @@
 DATESTAMP=$(date +%Y%m%d-%H%M%S)
 
 git add .
-git commit --allow-empty -am "jufaerma WIP at $(date +%Y%m%d-%H%M%S)"
-git push
+ git push
 
 
-git commit --allow-empty -am "jufaerma WIP at $(date +%Y%m%d-%H%M%S)" && git push
