ebaf2c783c0119cd277b80a8a0c8c1eb6ced297f
Julio Faerman <jufaerma@redhat.com>
2022-04-18 18:50:34 +0200

    jufaerma WIP at 20220418-185034

--- scripts/extract-install-dir.sh
+++ scripts/extract-install-dir.sh
@@ -4,4 +4,7 @@ export FILE_KEY=Masterkey123
 
 cd $HOME/Downloads/
 unzip install-dir.tar.gz.enc.zip
-openssl enc -e -a -aes256 -pbkdf2 -k ${FILE_KEY} -d -in "install-dir.tar.gz.enc" -out "install-dir.tar.gz"
+openssl enc -aes-256-cbc -d -a -pbkdf2 -k ${FILE_KEY} -in "install-dir.tar.gz.enc" -out "install-dir.tar.gz"
+mkdir install-dir
+tar zxvf "install-dir.tar.gz" -C install-dir
+export KUBECONFIG=$HOME/Downloads/install-dir/auth/kubeconfig
--- scripts/git-push-all.sh
+++ scripts/git-push-all.sh
@@ -3,5 +3,8 @@
 DATESTAMP=$(date +%Y%m%d-%H%M%S)
 
 git add .
-git commit --allow-empty -m "WIP at $DATESTAMP"
+git commit --allow-empty -am "jufaerma WIP at $(date +%Y%m%d-%H%M%S)"
 git push
+
+
+git commit --allow-empty -am "jufaerma WIP at $(date +%Y%m%d-%H%M%S)" && git push
